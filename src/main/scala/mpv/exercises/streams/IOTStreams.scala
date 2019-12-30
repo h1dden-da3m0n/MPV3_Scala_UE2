67a943032f417a7ff961be5b1468455dca8b857b
h1dden-da3m0n <33120068+h1dden-da3m0n@users.noreply.github.com>
2019-12-30 17:38:48 +0100

    add batch saving to mapAsync flow

--- src/main/scala/mpv/exercises/streams/IOTStreams.scala
+++ src/main/scala/mpv/exercises/streams/IOTStreams.scala
@@ -7,10 +7,10 @@ import akka.NotUsed
 import akka.actor.{ActorSystem, Cancellable}
 import akka.stream.scaladsl.{FileIO, Flow, Source}
 import akka.util.ByteString
-import mpv.exercises.actors.stream.PersistUtil.fileOpts
+import mpv.exercises.actors.stream.PersistUtil.{fileOpts, println}
 
-import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 import scala.util.Random
 
 object IOTStreams extends App {
@@ -19,7 +19,6 @@ object IOTStreams extends App {
 
   import system.dispatcher
 
-  private val weatherCsv = Paths.get("weatherData.csv")
   private val coreCnt = Runtime.getRuntime.availableProcessors
 
   case class WeatherReading(timestamp: LocalDateTime, temperature: Double)
@@ -27,11 +26,13 @@ object IOTStreams extends App {
   def generateWeatherSource(readSpeed: FiniteDuration,
                             readRange: (Double, Double)): Source[WeatherReading, Cancellable] = {
     Source.tick(readSpeed, readSpeed, "genData").map { _ =>
+      println("Source: GENERATING WeatherReading ...")
       WeatherReading(LocalDateTime.now(), Random.between(readRange._1, readRange._2))
     }
   }
 
   def weatherReadingMap(reading: WeatherReading): ByteString = {
+    println("Flow: MAPPING WeatherReading 2 ByteString ...")
     ByteString(f"${reading.timestamp},${reading.temperature}%.2f\n")
   }
 
@@ -46,23 +47,53 @@ object IOTStreams extends App {
 
   def generateBulkAsyncWeatherFlow(throttle: FiniteDuration = 0.millis,
                                    bulkSize: Int = 4): Flow[WeatherReading, ByteString, NotUsed] = {
+    def weatherReadingMap(reading: WeatherReading): String = {
+      println("Flow: MAPPING WeatherReading(s) 2 ByteString ...")
+      f"${reading.timestamp},${reading.temperature}%.2f"
+    }
+
+    def buildList(list: Seq[WeatherReading], elem: WeatherReading): Seq[WeatherReading] = {
+      list :+ elem
+    }
+
     if (throttle > 0.millis) {
-      Flow[WeatherReading].throttle(1, throttle).mapAsync(coreCnt)(x => Future {
-        weatherReadingMap(x)
-      })
+      Flow[WeatherReading].batch(bulkSize, buildList(Seq.empty[WeatherReading], _)) {
+        (readingSeq, reading) => buildList(readingSeq, reading)
+      }
+        .throttle(1, throttle)
+        .mapAsync(coreCnt)(readingSeq => Future {
+          ByteString(readingSeq.map(weatherReadingMap).mkString("\n"))
+        })
     }
     else {
-      Flow[WeatherReading].mapAsync(coreCnt)(x => Future {
-        weatherReadingMap(x)
-      })
+      Flow[WeatherReading].batch(bulkSize, buildList(Seq.empty[WeatherReading], _)) {
+        (readingSeq, reading) => buildList(readingSeq, reading)
+      }
+        .mapAsync(coreCnt)(readingSeq => Future {
+          ByteString(readingSeq.map(weatherReadingMap).mkString("\n"))
+        })
     }
   }
 
-  generateWeatherSource(250.millis, (12.0, 20.0))
-    .via(generateWeatherFlow())
-    .to(FileIO.toPath(weatherCsv, fileOpts))
-    .run()
+  def testStream(testName: String, fileName: String,
+                 source: => Source[WeatherReading, Cancellable],
+                 flow: => Flow[WeatherReading, ByteString, NotUsed]): Unit = {
+    val weatherCsv = Paths.get(fileName)
+    print(s"---- Testing $testName ----\n")
 
-  Thread.sleep(2000)
+    val f = source.limit(32).via(flow).runWith(FileIO.toPath(weatherCsv, fileOpts))
+
+    Await.ready(f, Duration.Inf)
+    Thread.sleep(50)
+    print("\n\n")
+  }
+
+  print("==== IOTAkkaStreams ====\n")
+  testStream("Default Flow", "weatherData_24b.csv",
+    generateWeatherSource(250.millis, (12.0, 20.0)), generateWeatherFlow(1.second))
+  testStream("Async Flow", "weatherData_24c.csv",
+    generateWeatherSource(250.millis, (12.0, 20.0)), generateBulkAsyncWeatherFlow(1.second))
+
+  Thread.sleep(200)
   system.terminate()
 }
